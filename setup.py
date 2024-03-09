2735db03db51bd28ac5e8dc5b11906850d08cb2a
Luis Guilherme Pelin Martins <lgpelin92@gmail.com>
2024-03-08 22:31:14 -0300

    [ENH] Enhance setup.py with additional metadata and project information
    * Add encoding declaration at the top of the file
    * Bump version number to 0.1.2
    * Include project URLs for bug tracker, documentation, and source code
    * Expand classifiers with development status, intended audience, license, operating system, and Python versions
    
    Co-authored-by: Comai <github.com/lguibr/comai>

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -5,14 +6,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="codeconcat",
-    version="0.1.1",
+    version="0.1.2",
     author="Luis Guilherme",
     author_email="lgpelin92@gmail.com",
     description="A tool to concatenate a codebase from a GitHub repository into a single text file",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    url="https://github.com/your-username/CodeConcat",
+    url="https://github.com/lguibr/CodeConcat",
+    project_urls={
+        "Bug Tracker": "https://github.com/lguibr/CodeConcat/issues",
+        "Documentation": "https://github.com/lguibr/CodeConcat/blob/main/README.md",
+        "Source Code": "https://github.com/lguibr/CodeConcat",
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
