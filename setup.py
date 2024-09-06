b73abce539d5e6245903e975339e155764062fb8
GitHub Action <action@github.com>
2024-09-06 18:01:05 +0000

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ url = "https://github.com/lguibr/CodeConcat"
 
 setup(
     name="codeconcat",
-    version="0.1.6",
+    version="0.1.7",
     author="Luis Guilherme",
     author_email="lgpelin92@gmail.com",
     description="A tool to concatenate a folders into a single text file",
